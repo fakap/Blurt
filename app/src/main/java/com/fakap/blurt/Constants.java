710c012c25758545f9f60025518d172b4e5cae56
Wojciech Olejnik <divoolej@gmail.com>
2016-03-19 00:19:29 +0100

    Add Constants class

---
+++ app/src/main/java/com/fakap/blurt/Constants.java
@@ -0,0 +1,11 @@
+package com.fakap.blurt;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by divoolej on 19/03/16.
+ */
+public class Constants {
+    public static final String FACEBOOK_ACCESS_TOKEN = "FACEBOOK_ACCESS_TOKEN";
+    public static Firebase firebaseReference;
+}
